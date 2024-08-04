20722f0343f6a826994022b775dd150c0c9ad3cb
HyuckJuneHong <jjj8337721@naver.com>
2024-08-04 16:15:08 +0900

    init: git setting

---
+++ src/test/java/kr/co/kafkaplayground/KafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kafkaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
