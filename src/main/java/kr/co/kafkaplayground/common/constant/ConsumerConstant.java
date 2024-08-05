8fdb9e2112b80f778f040e497a9133c542112779
HyuckJuneHong <jjj8337721@naver.com>
2024-08-05 20:15:26 +0900

    feat: 컨슈머 오토 커밋 예제 테스트

---
+++ src/main/java/kr/co/kafkaplayground/common/constant/ConsumerConstant.java
@@ -0,0 +1,25 @@
+package kr.co.kafkaplayground.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ConsumerConstant {
+
+	public static final String BOOTSTRAP_SERVERS = "bootstrap.servers";
+	public static final String BOOTSTRAP_SERVERS_LOCAL = "127.0.0.1:9092";
+	public static final String DESERIALIZER_KEY = "key.deserializer";
+	public static final String DESERIALIZER_VALUE = "value.deserializer";
+	public static final String DESERIALIZER_STRING = "org.apache.kafka.common.serialization.StringDeserializer";
+	public static final String GROUP_ID = "group.id";
+	public static final String GROUP_CONSUMER_01 = "peter-consumer01";
+	public static final String ENABLE_AUTO_COMMIT = "enable.auto.commit";
+	public static final String AUTO_OFFSET_RESET = "auto.offset.reset";
+	public static final String LATEST = "latest";
+	public static final String TOPIC_BASIC_01 = "peter-basic01";
+	public static final String TOPIC_MESSAGE = "======= Topic: ";
+	public static final String PARTITION_MESSAGE = "======= Partition: ";
+	public static final String OFFSET_MESSAGE = "======= Offset: ";
+	public static final String KEY_MESSAGE = "======= Key: ";
+	public static final String VALUE_MESSAGE = "======= Value: ";
+}
---
+++ src/main/java/kr/co/kafkaplayground/kafka/consumer/ConsumerAuto.java
@@ -0,0 +1,53 @@
+package kr.co.kafkaplayground.kafka.consumer;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import kr.co.kafkaplayground.common.constant.Constant;
+import kr.co.kafkaplayground.common.constant.ConsumerConstant;
+import kr.co.kafkaplayground.common.error.model.ErrorMessage;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConsumerAuto {
+
+	public static void main(String[] args) {
+		final Properties properties = generateProperties();
+
+		try (Consumer<String, String> consumer = new KafkaConsumer<>(properties)) {
+			consumer.subscribe(List.of(ConsumerConstant.TOPIC_BASIC_01));
+
+			while (true) {
+				ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+				for (ConsumerRecord<String, String> consumerRecord : records) {
+					log.info(ConsumerConstant.TOPIC_MESSAGE + consumerRecord.topic());
+					log.info(ConsumerConstant.PARTITION_MESSAGE + consumerRecord.partition());
+					log.info(ConsumerConstant.OFFSET_MESSAGE + consumerRecord.offset());
+					log.info(ConsumerConstant.KEY_MESSAGE + consumerRecord.key());
+					log.info(ConsumerConstant.VALUE_MESSAGE + consumerRecord.value() + Constant.NEWLINE);
+				}
+			}
+		} catch (Exception e) {
+			log.error(ErrorMessage.BASE.getMessage() + e);
+		}
+	}
+
+	private static Properties generateProperties() {
+		Properties properties = new Properties();
+		properties.put(ConsumerConstant.BOOTSTRAP_SERVERS, ConsumerConstant.BOOTSTRAP_SERVERS_LOCAL);
+		properties.put(ConsumerConstant.GROUP_ID, ConsumerConstant.GROUP_CONSUMER_01);
+		properties.put(ConsumerConstant.ENABLE_AUTO_COMMIT, true);
+		properties.put(ConsumerConstant.AUTO_OFFSET_RESET, ConsumerConstant.LATEST);
+		properties.put(ConsumerConstant.DESERIALIZER_KEY, ConsumerConstant.DESERIALIZER_STRING);
+		properties.put(ConsumerConstant.DESERIALIZER_VALUE, ConsumerConstant.DESERIALIZER_STRING);
+
+		return properties;
+	}
+}
